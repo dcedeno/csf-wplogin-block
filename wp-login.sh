775bd533cd4402e36c4d899583781b648a5a247c
dcedeno <dcedeno@gmail.com>
2017-12-18 21:20:01 -0500

    wplogin.sh
    Script SH

---
+++ wp-login.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+###start editing
+thold="100"
+btime="359m"
+###stop editing
+
+egrep 'wp-login.php' /usr/local/apache/domlogs/* | grep -v ftp_log | awk -F : '{print $2}' | awk '{print $1}' | sort | uniq -c | sort -n | awk -v limit="$thold" '$1 > limit{print $2}' > $$_ip_$$
+
+while IFS= read -r line
+        do
+                /usr/sbin/csf -td "$line"  "$btime" "bloqueado por ataque a Wordpress"
+        done < $$_ip_$$
+rm -f $$_ip_$$
